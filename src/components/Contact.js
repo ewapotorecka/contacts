3cccab7571d0194d0a27c0f309a41c8cd1b746ba
Ewa Potorecka <ewa.potorecka@gmail.com>
2021-03-07 19:56:28 +0100

    Add avatar for users without one

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,19 @@
-import { Avatar, Checkbox, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
+import { Checkbox, ListItem, ListItemAvatar, ListItemText } from '@material-ui/core';
 import React from 'react';
+import Img from "react-cool-img";
 
 export function Contact( { onClick, checkboxChecked, data } ) {
 
 	return (
 		<ListItem button onClick={ onClick }>
 			<ListItemAvatar>
-				<Avatar alt={ `avatar_${ data.data.first_name }_${ data.data.last_name }` } src={ data.data.avatar } />
+				<Img
+					alt={ `avatar_${ data.data.first_name }_${ data.data.last_name }` }
+					src={ data.data.avatar ?
+						data.data.avatar :
+						`https://eu.ui-avatars.com/api/?name=${ data.data.first_name }+${ data.data.last_name }` }
+					className='avatar'
+				/>
 			</ListItemAvatar>
 			<ListItemText primary={ `${ data.data.first_name } ${ data.data.last_name }` } secondary={ data.data.email } />
 			<Checkbox checked={ checkboxChecked } />
